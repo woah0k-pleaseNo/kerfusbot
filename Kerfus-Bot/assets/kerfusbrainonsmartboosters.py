fa36d8abb88a4dbaa0bbd26fce9bb2d187d0ce70
brbrbrbr <91349755+woah0k-pleaseNo@users.noreply.github.com>
2024-07-05 01:44:58 -0700

    Add files via upload

---
+++ Kerfus-Bot/assets/kerfusbrainonsmartboosters.py
@@ -0,0 +1,31 @@
+import sqlite3
+from discord import Embed, File, Color
+from discord.ext import commands
+from asyncio import run_coroutine_threadsafe
+
+
+
+def increment_user_data(ctx, column: str, increment: int)->int:
+    connection = sqlite3.connect('assets/kerfusmemorymachine.db')
+    cursor = connection.cursor()
+    with connection:
+        cursor.execute("SELECT * FROM users WHERE id=:id", {'id': ctx.author.id})
+        all_data = cursor.fetchall()
+        if len(all_data) < 1:
+            cursor.execute("INSERT INTO users VALUES (:id, 0, 0, 0)", {'id': ctx.author.id})
+        cursor.execute(f"SELECT {column} FROM users WHERE id=:id", {'id': ctx.author.id})
+        data = cursor.fetchone()
+        cursor.execute(f"UPDATE users SET {column}=:increment WHERE id=:id", {'increment': data[0]+increment, 'id': ctx.author.id})
+        return data[0]+increment
+
+
+async def embed_create(bot, ctx, embtitle: str, desc: str, *, thumbnail: str = None, img: str = None, clr = Color.blue()):
+        embmsg = Embed(title=embtitle, description=desc, color=clr)
+        if thumbnail != None:
+            thumbnailimg = File(thumbnail, filename=thumbnail[thumbnail.find('images/')+len('images/'):])
+            embmsg.set_thumbnail(url=f"attachment://{thumbnail[thumbnail.find('images/')+len('images/'):]}")
+            run_coroutine_threadsafe(ctx.send(file=thumbnailimg, embed=embmsg), bot.loop)
+        if img != None:
+            image = File(img, filename=img[img.find('selfies/')+len('selfies/'):])
+            embmsg.set_image(url=f"attachment://{img[img.find('selfies/')+len('selfies/'):]}")
+            run_coroutine_threadsafe(ctx.send(file=image, embed=embmsg), bot.loop)
---
+++ Kerfus-Bot/main.py
@@ -0,0 +1,48 @@
+import discord
+from sqlite3 import connect
+from discord.ext import commands
+from asyncio import run
+from os import listdir
+
+token_read = open('token', 'r')
+token = token_read.read()
+token_read.close()
+
+bot = commands.Bot(command_prefix="-kerfusbrother:", intents=discord.Intents.all(), help_command=None)
+run(bot.load_extension(f'assets.kerfusmindmachine'))
+
+connection = connect('assets/kerfusmemorymachine.db')
+cursor = connection.cursor()
+with connection:
+    cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+                   id integer, pets integer, shutups integer, friendship integer)""")
+
+
+
+@bot.event # starts when kerfus turns on
+async def on_ready():
+    print("-----------------\nKerfus has risen!\n-----------------")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="the sounds of the Swiss wilderness."))
+
+
+@bot.command(name='help')
+async def help(ctx):
+    help_string = """```
+    Commands:
+    - 'pet'/'pat'/'join': You pet Kerfus, which forces him into your voice channel.
+    - 'leave'/'getout'/'die': Kicks Kerfus out of your voice channel.
+    - 'sing'/'play'/'p': Make Kerfus sing a random Meowsynth song (except for that one non-Meowsynth song), also forces him into VC.
+    - 'skip'/'s'/'forward': Makes Kerfus sing a new random Meowsynth song.
+    - 'shutup': You are mean to Kerfus and it makes him stop singing.
+    - 'songs'/'list'/'songlist'/'listsongs': List all available songs which Kerfus can randomly choose from.
+    - 'selfie': Sends a random image of Kerfus.
+    - 'mood': Makes Kerfus talk about his feelings.
+
+    
+    - Brought to you by, your beloved, R 'brbrbrbr' D.
+    ```"""
+    await ctx.send(help_string)
+
+
+
+bot.run(token)
